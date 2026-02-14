df73454d853c7ff29ec3501cb9f4f06a95e70240
mirhuseynma <mirhuseynma@code.edu.az>
2026-02-14 19:26:00 +0400

    Update Program.cs

--- LAB4.Practice/Program.cs
+++ LAB4.Practice/Program.cs
@@ -7,7 +7,7 @@
             Techer teacher = new Techer(1545, "Mirhuseynma", new DateTime(2002, 6, 7), "IT", 800, 12);
             Student student1 = new Student(1234,"Mirhuseynma",new DateTime(2002,7,6),"234A",91);
             Console.WriteLine(student1.GetInfo());
-            student1.IsGraduated();
+            
             Console.WriteLine(teacher.GetInfo());
             Console.WriteLine(teacher.GetRole());
             teacher.CalculateRetirementYears();
