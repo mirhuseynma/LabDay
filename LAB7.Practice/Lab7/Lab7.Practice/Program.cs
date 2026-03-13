a61df591bbc9b9e2b69ad39f581d62851e860b57
mirhuseynma <mirhuseynma@code.edu.az>
2026-03-13 22:04:19 +0400

    AddLabDay7

---
+++ LAB7.Practice/Lab7/Lab7.Practice/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace Lab7.Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connect = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Product;Integrated Security=SSPI;";
+            SqlConnection connection = new SqlConnection(connect);
+            connection.Open();
+
+            GetAll(connect);
+        }
+
+        static void GetAll(string connect)
+        {
+            using SqlConnection connection = new SqlConnection(connect);
+            connection.Open();
+            string query = "SELECT * FROM Products";
+            using SqlCommand command = new SqlCommand(query, connection);
+            using SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(0));
+            }
+           
+            
+        }
+
+        
+        
+
+    }
+}
---
+++ LAB7.Practice/Lab7/Lab7.Practice/Service/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Data.SqlClient;
+
+namespace Lab7.Practice.Service
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
+
+        
+    }
+}
